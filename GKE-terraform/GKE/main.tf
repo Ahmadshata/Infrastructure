8e0573337c0469f11b595a0f6b46a03bd0aa7714
Ahmadshata <ahmadesmailshata@gmail.com>
2023-06-14 23:25:53 +0300

    Added workload identity

--- GKE-terraform/GKE/main.tf
+++ GKE-terraform/GKE/main.tf
@@ -4,19 +4,21 @@ resource "google_container_cluster" "primary" {
   location = var.cluster-zone
   network                  = var.vpc-uri
   subnetwork               = var.subnet-uri
+
   ip_allocation_policy {
-#the cluster's subnetwork to use for pod IP addresses.
-#Alternatively, cluster_ipv4_cidr_block can be used to automatically create a GKE-managed one.
+    #the cluster's subnetwork to use for pod and services IP addresses.
+    #Alternatively, cluster_ipv4_cidr_block can be used to automatically create a GKE-managed one.
     cluster_secondary_range_name  = "k8s-pod-range"
     services_secondary_range_name = "k8s-service-range"
   }
+
   private_cluster_config {
-    #enables intercommunication between nodes and master does not make the nodes public or anythnig
     enable_private_nodes    = true
     enable_private_endpoint = false
 
     master_ipv4_cidr_block  = var.master-cidr
   }
+
   master_authorized_networks_config {
     cidr_blocks {
     cidr_block   = var.authorized-cidr-1
@@ -31,15 +33,12 @@ resource "google_container_cluster" "primary" {
 #to configure Kubernetes workloads with GCP service accounts.
   workload_identity_config {
   workload_pool = "shata-387907.svc.id.goog"
-}
-
-  workload_metadata_config {
-      mode = "GKE_METADATA"
-    }
+  }
 
   release_channel {
     channel = "REGULAR"
   }
+
   remove_default_node_pool = true
   initial_node_count       = 1
 }
@@ -58,6 +57,9 @@ resource "google_container_node_pool" "node-pool" {
   node_config {
     preemptible  = false
     machine_type = var.node-pool-machine_type
+    workload_metadata_config {
+      mode = "GKE_METADATA"
+  }
   }
 }
 
